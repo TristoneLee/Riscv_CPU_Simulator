03ebb6dbba9053013aa71ac3a63f6ec0d0e7bd80
Lukeee <trileereize@gmail.com>
2022-07-04 16:44:44 +0800

    test version

--- src/components/ReorderBuffer.hpp
+++ src/components/ReorderBuffer.hpp
@@ -34,7 +34,7 @@ public:
             registers), load_store_unit_(load_store_unit), buffer_(), PC_(PC), issue_queue_(issue_queue) {}
 
     int push(UOP uop) {
-        if (uop.type == U || uop.name == JAL || uop.name == END || uop.name == RET)
+        if (uop.type == U || uop.name == JAL || uop.name == END )
             return buffer_.push({true, uop, uop.imm});
         else return buffer_.push({false, uop, 0});
     }
@@ -51,8 +51,6 @@ public:
             cout << (((unsigned int) (*registers_)[registers_->register_snapshots.front().rename_map_table_[10]]) &
                      255u) << endl;
             throw 1;
-        } else if (uop.name == RET) {
-            *PC_ = (*registers_)[registers_->register_snapshots.front().rename_map_table_[1]];
         } else if (uop.type == R || uop.type == I && uop.name != JALR || uop.name == LUI) {
             registers_->Write(front_reorder.result, uop.rd);
             issue_queue_->UpdateStatus(uop.rd);
@@ -94,7 +92,7 @@ public:
                 throw "PC_reset";
             } else registers_->Reflush();
         } else if (uop.name == BLTU) {
-            if ((unsigned) registers_->Read(uop.rs1) <= (unsigned) registers_->Read(uop.rs2)) {
+            if ((unsigned) registers_->Read(uop.rs1) < (unsigned) registers_->Read(uop.rs2)) {
                 *PC_ = uop.PC + uop.imm;
                 registers_->Pop();
                 throw "PC_reset";
